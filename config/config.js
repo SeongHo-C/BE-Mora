c193114cdcfddf164a139096bc5d3fff8b3247d5
[SW4/1반] 이성호 <leeseong7130@gmail.com>
2023-06-01 23:51:36 +0900

    Fix: 잘못된 폴더 경로 수정

--- config/config.js
+++ config/config.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: 'root',
-    password: null,
-    database: 'database_development',
+    password: '',
+    database: '',
     host: '127.0.0.1',
     dialect: 'mysql',
   },
