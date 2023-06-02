0cae7cc8f4761639da8d968de5d06ef095260999
[SW4/1반] 이혜정 <pinklady1018@gmail.com>
2023-06-03 00:38:23 +0900

    Feat: 프로젝트 환경 스웨거 설정, 미들웨어 추가

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const dotenv = require('dotenv');
+const cors = require('cors');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger-output');
 
 dotenv.config();
 const { sequelize } = require('./models');
@@ -18,10 +22,12 @@ sequelize
     console.error(err);
   });
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
