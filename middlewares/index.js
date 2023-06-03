a6eb8a6221d5c6ee66292f2d5c4fa3c748a9d640
[SW4/1반] 이혜정 <pinklady1018@gmail.com>
2023-06-03 18:27:31 +0900

    Fix: 미들웨어 async-handler, 미들웨어 index.js 수정

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,5 +1,5 @@
-const { asyncHandler } = require('./async-handler');
-const { errorHandler } = require('./error-handler');
-const { loginRequired } = require('./login-required');
+const asyncHandler = require('./async-handler');
+const errorHandler = require('./error-handler');
+const loginRequired = require('./login-required');
 
 module.exports = { asyncHandler, errorHandler, loginRequired };
