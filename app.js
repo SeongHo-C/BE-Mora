568cb81c93d9929a755ad4b58a025b46df0a17ce
[SW4/1반] 이혜정 <pinklady1018@gmail.com>
2023-06-07 20:35:15 +0900

    Fix: cors 에러 수정

--- app.js
+++ app.js
@@ -31,16 +31,19 @@ sequelize
   });
 
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-app.get('/api', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.send(data);
-});
 
 app.use('/api', adminRouter);
 app.use('/api', noticeRouter);
