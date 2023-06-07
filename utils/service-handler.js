c92dc6b01369a9422fe00f3345cfb8f0d5cd44a7
[SW4/1반] 이혜정 <pinklady1018@gmail.com>
2023-06-07 23:53:31 +0900

    Fix: service-handler 수정

--- utils/service-handler.js
+++ utils/service-handler.js
@@ -1,10 +1,13 @@
+const { BadRequestClass } = require('../middlewares');
+
 const serviceHandler = (service) => {
   return async (req, res, next) => {
     try {
       await service(req, res);
       next();
     } catch (err) {
-      next(err);
+      console.error(err);
+      throw new BadRequestClass('BAD REQUREST ERROR');
     }
   };
 };
