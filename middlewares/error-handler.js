1768897ce0eb41985f94bbeaa123e9b770f1d4e6
[SW4/1반] 이혜정 <pinklady1018@gmail.com>
2023-06-12 23:45:12 +0900

    Fix: 에러핸들러 수정

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -2,5 +2,5 @@ const logger = require('../logger');
 
 module.exports = (err, req, res, next) => {
   logger.error(err);
-  res.status(err.code).json({ message: err.message });
+  res.status(err.code).json({ message: err.message, statusCode: err.code });
 };
