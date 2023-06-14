777af8f698956dc3c7c175406ae8db69d87011d1
SeongHo-C <leeseong7130@gmail.com>
2023-06-15 03:39:29 +0900

    Refactor: Comment api 리팩터링

--- api/comment/controller.js
+++ api/comment/controller.js
@@ -5,23 +5,26 @@ module.exports = {
     const { content, board_id } = req.body;
     const commenter = req.currentId;
 
-    await commentService.setComment(content, board_id, commenter);
-    res.status(201).json('댓글 작성 성공');
+    res
+      .status(201)
+      .json(await commentService.setComment(content, board_id, commenter));
   },
 
   async updateComment(req, res) {
     const { content, comment_id } = req.body;
     const userId = req.currentId;
 
-    await commentService.updateComment(content, comment_id, userId);
-    res.status(200).json('댓글 수정 완료');
+    res
+      .status(200)
+      .json(await commentService.updateComment(content, comment_id, userId));
   },
 
   async deleteComment(req, res) {
     const { comment_id } = req.body;
     const userId = req.currentId;
 
-    await commentService.deleteComment(comment_id, userId);
-    res.status(200).json('댓글 삭제 완료');
+    res
+      .status(200)
+      .json(await commentService.deleteComment(comment_id, userId));
   },
 };
